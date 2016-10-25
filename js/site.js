2a821622ed064964a6a44539895e8c73e1ec66b1
George Bora <bora.george@yahoo.com>
2016-10-25 23:34:49 +0300

    working on front page

---
+++ js/site.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.first-slider').slick({
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+      prevArrow: false,
+      nextArrow: false,
+      autoplay: true,
+      autoplaySpeed: 1000
+  });
+});
+	
